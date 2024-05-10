435ccaa58fdd7a8a20c4dc791a5cc3ec374c2359
AbhishekKS28 <1ksabhishek@gmail.com>
2024-05-10 23:34:42 +0530

    implemented opening app by notificatin by adding deepl >> inking and pending intent

--- app/src/main/java/com/abhi/expencetracker/Notifications/NotificationService.kt
+++ app/src/main/java/com/abhi/expencetracker/Notifications/NotificationService.kt
@@ -1,37 +1,48 @@
 package com.abhi.expencetracker.Notifications
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import com.abhi.expencetracker.MainActivity
 import com.abhi.expencetracker.R
 import kotlin.random.Random
 
 class NotificationService(
-    private val context: Context ,
-    val notificationDescription :String
+    private val context: Context,
+    val notificationDescription: String
 ) {
 
-   private val notificationManager = context.getSystemService(NotificationManager::class.java)
+    private val notificationManager = context.getSystemService(NotificationManager::class.java)
 
     @RequiresApi(Build.VERSION_CODES.N)
-    fun showDailyNotification(){
+    fun showDailyNotification() {
 
+        val intent = Intent(context, MainActivity::class.java) // Explicit intent to MainActivity
 
-        val notification = NotificationCompat.Builder(context, "Daily_Remainder" //channel created in MainApplication not in MainActivity
-        ).setContentTitle("Expense Tracker")
-            .setContentText(notificationDescription) //Description of notification
+        // Flags for a new task and clearing the previous task (consider alternatives if needed)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_MUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(context, "Daily_Remainder")
+            .setContentTitle("Expense Tracker")
+            .setContentText(notificationDescription)
             .setSmallIcon(R.drawable.add_transation_notification)
             .setPriority(NotificationManager.IMPORTANCE_HIGH)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent) // Set the PendingIntent for notification click
             .build()
-
 
         notificationManager.notify(
             Random.nextInt(),
             notification
         )
-
     }
 }
