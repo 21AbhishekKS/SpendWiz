f779a601be26196d98e97038f4f225bc5be224db
AbhishekKS28 <1ksabhishek@gmail.com>
2025-09-08 17:31:19 +0530

    version updated for playstore release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abhi.expencetracker"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.3"
+        versionCode = 12
+        versionName = "1.5.9"
 
 
 
